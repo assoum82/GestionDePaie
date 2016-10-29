3e92eb2b864704669b703d25eaef3d13e266f9ff
Asmaa Bengueddach <asmaa.bengueddach@gmail.com>
2016-10-29 20:36:55 +0200

    T Bien
    essaie de voir https://github.com/assoum82/Fiche-TD-n4.git

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Employe> employes = new ArrayList<>();
 
-        employes.add(new Employe("Chawki", "PDG", 500000));
+        employes.add(new Employe("Chawki", "PDG", 500000)); //hahahaha
         employes.add(new EmployePleinTemps("Mohammed", "Secretaire", 5500, 1000));
         employes.add(new EmployeTempsPartiel("Isak", "Agent de securité ", 750, 20));
 
